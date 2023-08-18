b9891d00fcd2241e443bab6a6d194abb8fbda811
InGyu Moon <ansdlsrb1121@naver.com>
2023-08-18 18:37:53 +0900

    add test html

--- src/main/java/m22t/ansdlsrb/m22tProject/controller/PlaceController.java
+++ src/main/java/m22t/ansdlsrb/m22tProject/controller/PlaceController.java
@@ -5,22 +5,20 @@ import m22t.ansdlsrb.m22tProject.data.dto.PlaceDto;
 import m22t.ansdlsrb.m22tProject.service.PlaceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping()
 public class PlaceController {
     private final PlaceService placeService;
     @Autowired
     public PlaceController(PlaceService placeService) {
         this.placeService = placeService;
     }
-    @PostMapping
+    @PostMapping("/search-places")
     public ResponseEntity<List<PlaceDto>> searchPlacesByCoordinatesAndType(@RequestBody FrontendDto frontendDto) {
         List<PlaceDto> places = placeService.getPlacesByCoordinates(frontendDto);
         return ResponseEntity.ok(places);
--- src/main/java/m22t/ansdlsrb/m22tProject/service/PlaceService.java
+++ src/main/java/m22t/ansdlsrb/m22tProject/service/PlaceService.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface PlaceService {
     List<PlaceDto> getPlacesByCoordinates(FrontendDto frontendDto);
+
 }
