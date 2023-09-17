4e894c58cacaff5c0c07c48023b6949cd106f0a9
wootaepark <pakdal99@gmail.com>
2023-09-17 22:21:05 +0900

    0917_commit

--- src/main/java/m22t/ansdlsrb/m22tProject/controller/PlaceController.java
+++ src/main/java/m22t/ansdlsrb/m22tProject/controller/PlaceController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/")
 public class PlaceController {
     private final PlaceService placeService;
 
--- src/main/java/m22t/ansdlsrb/m22tProject/controller/ReserveController.java
+++ src/main/java/m22t/ansdlsrb/m22tProject/controller/ReserveController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/")
 public class ReserveController {
 
     private final ReservationRepository reservationRepository;
